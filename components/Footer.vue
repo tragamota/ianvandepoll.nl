82bed509e09c4e36b1c172f11e8c818a75d0e410
Ian van de Poll <ian.vd.poll@gmail.com>
2024-03-29 21:41:06 +0100

    Added temporary spacing in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,20 +1,26 @@
 <template>
-    <footer class="footer">
-        <p>Copyright 2024 - Ian van de Poll</p>
-    </footer>
+  <footer class="footer">
+    <div class="footer-container">
+      <p>Copyright 2024 - Ian van de Poll</p>
+    </div>
+  </footer>
 </template>
-
 
 <style scoped>
 .footer {
-    display: block;
-    position: relative;
+  display: block;
+  position: relative;
 
-    height: 2rem;
+  height: 4rem;
 }
 
 .footer-container {
+  display: flex;
+  position: relative;
 
+  height: 100%;
+  margin-left: 4rem;
+
+  align-items: center;
 }
-
 </style>
