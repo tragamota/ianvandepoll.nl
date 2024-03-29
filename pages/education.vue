d81feb80fd54f7d6880c896f666bdb935f36ddd0
Ian van de Poll <ian.vd.poll@gmail.com>
2024-03-29 21:38:40 +0100

    Add Page titles

---
+++ pages/education.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="content-container">
+        <h1>Education</h1>
+    </div>
+</template>
---
+++ pages/experience.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="content-container">
+        <h1>Experience</h1>
+    </div>
+</template>
