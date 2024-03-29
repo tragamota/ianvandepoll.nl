1a36ec2b9e63c2e8a998eccff06740cfb6d7d65c
Ian van de Poll <ian.vd.poll@gmail.com>
2024-03-29 21:39:34 +0100

    Added overall layout of header

--- components/Header.vue
+++ components/Header.vue
@@ -2,15 +2,57 @@ import type { NuxtLink } from '#build/components';
 <template>
   <header class="header">
     <div class="header-container">
+      <div class="header-menu">
+        <img class="header-menu-icon" src="~/assets/images/menu-icon.svg">
+      </div>
+
       <NuxtLink to="/" class="header-logo">
-        <span>Van de Poll</span>  
-      </NuxtLink> 
+        <span>Van de Poll</span>
+      </NuxtLink>
 
       <div class="header-routes">
-        <ul>
-          <NuxtLink to="/"> Home </NuxtLink>
+        <ul class="header-list">
+          <li class="header-item">
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li class="header-item">
+            <NuxtLink to="/about">About me</NuxtLink>
+          </li>
+          <li class="header-item">
+            <NuxtLink to="/education">Education</NuxtLink>
+          </li>
+          <li class="header-item">
+            <NuxtLink to="/experience">Experience</NuxtLink>
+          </li>
+        </ul>
+
+        <div class="header-divider"></div>
+
+        <ul class="header-list">
+          <li class="header-item">
+            <NuxtLink to="/blog">Blog</NuxtLink>
+          </li>
+          <li class="header-item">
+            <NuxtLink to="/contact">Contact</NuxtLink>
+          </li>
         </ul>
       </div>
+
+      <ul class="header-socials">
+        <li class="header-item">
+          <a
+            href="https://www.linkedin.com/in/ian-v-a64845158/"
+            target="_blank"
+          >
+            <img class="header-icon" src="~/assets/images/linkedin-icon.png" />
+          </a>
+        </li>
+        <li class="header-item">
+          <a href="https://github.com/tragamota" target="_blank">
+            <img class="header-icon" src="~/assets/images/github-icon.svg" />
+          </a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
@@ -38,7 +80,25 @@ import type { NuxtLink } from '#build/components';
   flex-direction: row;
 
   width: 100%;
-  min-height: 3rem;
+  height: 4.5rem;
+}
+
+.header-menu {
+  display: block;
+  position: relative;
+
+  margin-left: 1rem;
+}
+
+.header-menu-icon {
+  display: none;
+  position: relative;
+
+  visibility: collapse;
+
+  width: 38px;
+
+  color:#627d97;
 }
 
 .header-logo {
@@ -46,13 +106,15 @@ import type { NuxtLink } from '#build/components';
   position: relative;
 
   height: 100%;
-  
+
   margin-left: 10%;
-  padding: 1rem 2rem 1rem 2rem;
+  margin-right: 2rem;
+  padding: 1.25rem 2.5rem 1.25rem 2.5rem;
 
   text-decoration: none;
+  text-wrap: nowrap;
 
-  background: #8b8b8b;
+  background: #7393b3;
 }
 
 .header-logo > span {
@@ -65,9 +127,84 @@ import type { NuxtLink } from '#build/components';
 }
 
 .header-routes {
+  display: flex;
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+  padding-left: 1rem;
+  padding-right: 1.5rem;
+
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.header-socials {
+  display: flex;
+  position: relative;
+
+  height: 100%;
+
+  align-items: center;
+
+  padding: 0;
+}
+
+.header-list {
   display: block;
   position: relative;
 
+  padding: 0 0.5rem 0 0.5rem;
+  margin: 0;
+
+  list-style: none;
+}
+
+.header-item {
+  display: inline-block;
+  position: relative;
+
+  margin-right: 1rem;
+}
+
+.header-item > a {
+  color: #7393b3;
+
+  text-decoration: none;
+  text-transform: uppercase;
+
+  font-size: 1.25rem;
+}
+
+.header-item > a:active {
+  color: #627d97;
+
+  text-decoration: underline;
+}
+
+.header-item > a:hover {
+  color: #627d97;
+
+  text-decoration: underline;
+}
+
+.header-icon {
+  display: inline-block;
+
+  width: 26px;
+}
+
+.header-divider {
+  display: inline-block;
+
+  height: 40%;
+
+  border-left: 2px solid #7393b39c;
+
+  margin: 0 1rem 0 0.5rem;
+}
+
+@media screen and (min-width: 1400px) {
 
 }
 </style>
