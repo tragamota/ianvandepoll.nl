79e4d9809c3e20d9bd2e9f004a44625d9cbe5e09
Ian van de Poll <ian.vd.poll@gmail.com>
2024-07-23 01:49:58 +0200

    Responsive header is working needs styling on mobile

--- components/Header.vue
+++ components/Header.vue
@@ -1,210 +1,346 @@
-import type { NuxtLink } from '#build/components';
 <template>
   <header class="header">
     <div class="header-container">
-      <div class="header-menu">
-        <img class="header-menu-icon" src="~/assets/images/menu-icon.svg">
+      <div class="header-menu" @click="toggleMenu">
+        <img
+          v-if="!menuOpen"
+          src="~/assets/images/menu-icon.svg"
+          class="header-menu-icon"
+          alt="Menu icon"
+        />
+        <img
+          v-else
+          src="~/assets/images/close-icon.svg"
+          class="header-menu-icon"
+          alt="Close icon"
+        />
       </div>
 
       <NuxtLink to="/" class="header-logo">
         <span>Van de Poll</span>
       </NuxtLink>
 
-      <div class="header-routes">
-        <ul class="header-list">
-          <li class="header-item">
+      <div class="header-content">
+        <ul class="header-routes">
+          <li class="header-route">
             <NuxtLink to="/">Home</NuxtLink>
           </li>
-          <li class="header-item">
+          <li class="header-route">
             <NuxtLink to="/about">About me</NuxtLink>
           </li>
-          <li class="header-item">
+          <li class="header-route">
             <NuxtLink to="/education">Education</NuxtLink>
           </li>
-          <li class="header-item">
+          <li class="header-route">
             <NuxtLink to="/experience">Experience</NuxtLink>
           </li>
         </ul>
 
         <div class="header-divider"></div>
 
-        <ul class="header-list">
-          <li class="header-item">
+        <ul class="header-extras">
+          <li class="header-route">
             <NuxtLink to="/blog">Blog</NuxtLink>
           </li>
-          <li class="header-item">
+          <li class="header-route">
             <NuxtLink to="/contact">Contact</NuxtLink>
           </li>
         </ul>
-      </div>
 
-      <ul class="header-socials">
-        <li class="header-item">
-          <a
-            href="https://www.linkedin.com/in/ian-v-a64845158/"
-            target="_blank"
-          >
-            <img class="header-icon" src="~/assets/images/linkedin-icon.png" />
-          </a>
+        <ul class="header-socials">
+          <li class="header-route">
+            <a
+              href="https://www.linkedin.com/in/ian-v-a64845158/"
+              target="_blank"
+            >
+              <img
+                class="header-icon"
+                src="~/assets/images/linkedin-icon.png"
+              />
+            </a>
+          </li>
+          <li class="header-route">
+            <a href="https://github.com/tragamota" target="_blank">
+              <img class="header-icon" src="~/assets/images/github-icon.svg" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div
+      class="header-mobile-content"
+      :class="{ 'header-mobile-content-open': menuOpen }"
+    >
+      <ul class="header-mobile-routes">
+        <li class="header-mobile-route">
+          <NuxtLink to="/">Home</NuxtLink>
         </li>
-        <li class="header-item">
-          <a href="https://github.com/tragamota" target="_blank">
-            <img class="header-icon" src="~/assets/images/github-icon.svg" />
-          </a>
+        <li class="header-mobile-route">
+          <NuxtLink to="/about">About me</NuxtLink>
+        </li>
+        <li class="header-mobile-route">
+          <NuxtLink to="/education">Education</NuxtLink>
+        </li>
+        <li class="header-mobile-route">
+          <NuxtLink to="/experience">Experience</NuxtLink>
+        </li>
+      </ul>
+
+      <div class="header-mobile-divider"></div>
+
+      <ul class="header-mobile-extras">
+        <li class="header-mobile-route">
+          <NuxtLink to="/blog">Blog</NuxtLink>
+        </li>
+        <li class="header-mobile-route">
+          <NuxtLink to="/contact">Contact</NuxtLink>
         </li>
       </ul>
     </div>
   </header>
 </template>
 
+<script lang="ts">
+export default defineComponent({
+  name: "Header",
+  setup() {
+    const menuOpen = ref(false);
+
+    const toggleMenu = () => {
+      menuOpen.value = !menuOpen.value;
+    };
+
+    return {
+      menuOpen,
+      toggleMenu,
+    };
+  },
+});
+</script>
+
 <style scoped lang="scss">
 .header {
   display: block;
   position: sticky;
-
   top: 0;
   left: 0;
   right: 0;
-
   background-color: #fff;
-
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.15);
   z-index: 1000;
 }
 
 .header-container {
   display: flex;
   position: relative;
-
   align-items: center;
   flex-direction: row;
-
   width: 100%;
   height: 4.5rem;
 }
 
 .header-menu {
-  display: block;
+  display: none;
   position: relative;
-
   margin-left: 1rem;
+  cursor: pointer;
 }
 
 .header-menu-icon {
-  display: none;
-  position: relative;
-
-  visibility: collapse;
-
   width: 38px;
+  height: 38px;
+  transition: transform 0.5s ease, opacity 0.5s ease;
+}
 
-  color:#627d97;
+.header-menu-icon-enter-active,
+.header-menu-icon-leave-active {
+  transition: transform 0.5s ease, opacity 0.5s ease;
+}
+
+.header-menu-icon-enter,
+.header-menu-icon-leave-to {
+  transform: rotate(90deg);
+  opacity: 0;
 }
 
 .header-logo {
   display: block;
   position: relative;
-
   height: 100%;
-
-  margin-left: 10%;
+  margin-left: 5rem;
   margin-right: 2rem;
-  padding: 1.25rem 2.5rem 1.25rem 2.5rem;
-
+  padding: 1.25rem 2.5rem;
   text-decoration: none;
   text-wrap: nowrap;
-
   background: #7393b3;
 }
 
 .header-logo > span {
   text-transform: uppercase;
-
   font-size: 20pt;
   font-weight: bold;
-
   color: #fff;
 }
 
-.header-routes {
+.header-content {
   display: flex;
   position: relative;
-
   width: 100%;
   height: 100%;
   padding-left: 1rem;
-  padding-right: 1.5rem;
-
+  padding-right: 1rem;
   justify-content: flex-end;
   align-items: center;
+}
+
+.header-routes {
+  display: block;
+  position: relative;
+  padding: 0 0.5rem;
+  margin: 0;
+  list-style: none;
+}
+
+.header-extras {
+  display: block;
+  position: relative;
+  padding: 0 0.5rem;
+  margin: 0 1rem 0 0;
+  list-style: none;
 }
 
 .header-socials {
   display: flex;
   position: relative;
-
   height: 100%;
-
   align-items: center;
-
-  padding: 0;
+  padding-left: 1rem;
 }
 
-.header-list {
-  display: block;
-  position: relative;
-
-  padding: 0 0.5rem 0 0.5rem;
-  margin: 0;
-
-  list-style: none;
-}
-
-.header-item {
+.header-route {
   display: inline-block;
   position: relative;
-
   margin-right: 1rem;
 }
 
-.header-item > a {
+.header-route > a {
   color: #7393b3;
-
   text-decoration: none;
   text-transform: uppercase;
-
   font-size: 1.25rem;
 }
 
-.header-item > a:active {
-  color: #627d97;
+.header-route:last-child {
+  margin-right: 0;
+}
 
+.header-route > a:active {
+  color: #627d97;
   text-decoration: underline;
 }
 
-.header-item > a:hover {
+.header-route > a:hover {
   color: #627d97;
-
   text-decoration: underline;
 }
 
 .header-icon {
   display: inline-block;
-
   width: 26px;
 }
 
 .header-divider {
   display: inline-block;
-
   height: 40%;
-
   border-left: 2px solid #7393b39c;
-
-  margin: 0 1rem 0 0.5rem;
+  margin: 0 1rem 0 1rem;
 }
 
-@media screen and (min-width: 1400px) {
+.header-mobile-content {
+  display: none;
+  flex-direction: column;
+  padding-left: 0;
+  padding-right: 0;
+}
 
+.header-mobile-route {
+  @extend .header-route
+}
+
+.header-mobile-extras {
+  @extend .header-extras
+}
+
+@media screen and (max-width: 768px) {
+  .header-menu {
+    display: block;
+  }
+
+  .header-divider {
+    display: none;
+  }
+
+  .header-routes {
+    display: none;
+  }
+
+  .header-extras {
+    display: none;
+  }
+
+  .header-logo {
+    margin-left: 1rem;
+    margin-right: 0;
+    padding: 1.25rem 1.5rem;
+  }
+
+  .header-mobile-content-open {
+    display: block;
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .header-menu {
+    display: block;
+  }
+
+  .header-mobile-route {
+    display: block;
+
+    text-align: center;
+
+    margin-right: 0;
+    margin-bottom: 1rem;
+  }
+
+  .header-mobile-route:first-child {
+    margin-top: 1rem;
+  }
+
+  .header-mobile-route:last-child {
+    margin-bottom: 0;
+  }
+
+  .header-mobile-divider {
+    display: none;
+  }
+
+  .header-divider {
+    display: none;
+  }
+
+  .header-routes {
+    display: none;
+  }
+
+  .header-logo {
+    margin-left: 1rem;
+    padding: 1.25rem 1.5rem;
+  }
+
+  .header-mobile-content-open {
+    display: block;
+  }
 }
 </style>
